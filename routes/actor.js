c0a147db772d5ea312b62dea3edcab727653df3d
Elijah <whiteelijah.cc@gmail.com>
2023-06-01 09:35:54 -0400

    minor tweaks

--- routes/actor.js
+++ routes/actor.js
@@ -1,12 +1,12 @@
 //import
 const express = require("express");
-const router = express.Router();
+const route = express.Route();
 const actorsController = require("../controllers/actor.js");
 
 //actors
-router.get('/actor/:name', controller.getActor) 
-router.post('/actors/:name', controller.addActor)
-router.put ('/actors/:name', controller.updateActor)
-router.delete('/actors/:name',controller.deleteActor) 
+route.get('/actor/:name', controller.getActor) 
+route.post('/actors/:name', controller.addActor)
+route.put ('/actors/:name', controller.updateActor)
+route.delete('/actors/:name',controller.deleteActor) 
 
-module.exports = router;
+module.exports = route;
