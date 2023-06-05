7b835df0c20801d4d9f2782bd4347660b4afabb4
Elijah <whiteelijah.cc@gmail.com>
2023-06-05 01:36:52 -0400

    server.js solution to error

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://papaya-mermaid-89cef7.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.use(express.json());
 
 // Register movie routes
